cf8f171a831505b464f76f52f1a86473a4f5dcc2
Blake Carter <s4325722@student.uq.edu.au>
2014-03-20 18:35:05 +1000

    Work on Places API

--- Practical 1/Gruntfile.js
+++ Practical 1/Gruntfile.js
@@ -66,7 +66,8 @@ module.exports = function(grunt) {
             production: {
                 files: [
                     // includes files within path
-                    {expand: true, cwd: 'src/', src: ['*.html'], dest: 'build/', filter: 'isFile'}
+                    {expand: true, cwd: 'src/', src: ['*.html'], dest: 'build/', filter: 'isFile'},
+                    {expand: true, cwd: 'src/dart/packages/browser', src: ['dart.js'], dest: 'build/assets/js', filter: 'isFile'}
                 ]
             }
         }
--- Practical 1/src/dart/main.dart
+++ Practical 1/src/dart/main.dart
@@ -6,17 +6,17 @@ GMap map;
 InfoWindow infowindow;
 
 void main(){
-  final pyrmont = new LatLng(-33.8665433, 151.1956316);
+  final st_lucia = new LatLng(-27.5000, 153.0000);
 
   map = new GMap(querySelector("#map"), new MapOptions()
     ..mapTypeId = MapTypeId.ROADMAP
-    ..center = pyrmont
+    ..center = st_lucia
     ..zoom = 15
   );
 
   final request = new PlaceSearchRequest()
-    ..location = pyrmont
-    ..radius = 500
+    ..location = st_lucia
+    ..radius = 1500
     ..types = ['store']
   ;
 
@@ -29,7 +29,31 @@ void main(){
 void callback(List<PlaceResult> results, PlacesServiceStatus status, PlaceSearchPagination pagination) {
   if (status == PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
-      createMarker(results[i]);
+
+      if(results[i].photos == null || results[i].photos.length == 0)
+        continue;
+
+      for(var j = 0; j < results[i].photos.length; j++) {
+        PlacePhoto pp = results[i].photos[j];
+
+        var img = new ImageElement(
+            src: "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference="// + pp.$unsafe['photo_reference']
+        );
+
+        querySelector("#images").nodes.add(img);
+      }
     }
   }
+}
+
+void randomiseGrid(int gridColumns, int gridRows){
+  var scaleSteps = 3;
+
+
+
+
+}
+
+class CellSize {
+  constructor(public width, public height){}
 }
