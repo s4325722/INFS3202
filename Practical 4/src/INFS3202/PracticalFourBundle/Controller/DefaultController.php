b506799081332d75f12917fbd3ad1b7f36377419
Blake Carter <s4325722@student.uq.edu.au>
2014-05-12 04:42:37 +1000

    Building forms

--- Practical 4/src/INFS3202/PracticalFourBundle/Controller/DefaultController.php
+++ Practical 4/src/INFS3202/PracticalFourBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace INFS3202\PracticalFourBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -14,6 +15,23 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
+        $deals = $this->getDoctrine()->getRepository('INFS3202PracticalFourBundle:Deal')->findAll();
+
+        return array('deals' => $deals);
+    }
+
+    /**
+     * @Route("/login")
+     * @Template()
+     */
+    public function loginAction()
+    {
+        $request = $this->get('request');
+
+        if($request->getMethod() == 'POST'){
+
+        }
+        
         $deals = $this->getDoctrine()->getRepository('INFS3202PracticalFourBundle:Deal')->findAll();
 
         return array('deals' => $deals);
