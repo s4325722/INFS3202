4dec400841a0ad978b323ebeeffad688b6316417
Blake Carter <s4325722@student.uq.edu.au>
2014-05-12 02:07:15 +1000

    Added foundation

--- Practical 4/app/AppKernel.php
+++ Practical 4/app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new INFS3202\PracticalFourBundle\INFS3202PracticalFourBundle(),
+            new Bmatzner\FoundationBundle\BmatznerFoundationBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
